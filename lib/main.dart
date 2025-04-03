3a5302678f09e8cc1443101840cedfd8ee5d6797
VALENTIN_LOYA__ <a.22308051281244@cbtis128.edu.mx>
2025-04-03 18:40:55 +0000

    gy

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,95 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MiPagina());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiPagina extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false, // Quita el banner de debug
+      home: Scaffold(
+        backgroundColor: Colors.orange,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Logo (Imagen redonda)
+              Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red, // Fondo rojo para el círculo del logo
+                ),
+                child: Center(
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/vale.jpg', // Asegúrate de tener la imagen en tu carpeta assets
+                      width:
+                          350, // Ajusta el tamaño de la imagen dentro del círculo
+                      height:
+                          350, // Ajusta el tamaño de la imagen dentro del círculo
+                      fit: BoxFit
+                          .cover, // Para que la imagen cubra todo el círculo
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                'Bienvenido',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color:
+                      Color(0xff000000), // Cambia el color de la letra a blanco
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  // Lógica para iniciar sesión
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Iniciar Sesion',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color:
+                          Colors.white), // Cambia el color de la letra a blanco
+                ),
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {
+                  // Lógica para crear cuenta
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Crear Cuenta',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color:
+                          Colors.white), // Cambia el color de la letra a blanco
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
